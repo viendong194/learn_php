fa397476d4eae89e1176ae7b971bdd29978e5aea
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-08-28 16:35:02 +0900

    add pagination in view_users

--- view_users.php
+++ view_users.php
@@ -4,9 +4,28 @@
   echo '<h1>Registerd Users</h1>';
   // connect to db
   require('./mysql_connect.php');
+  $display = 2;
+  if(isset($_GET['p']) && is_numeric($_GET['p'])){
+    $pages = $_GET['p'];
+  }else{
+    $q = "SELECT COUNT(user_id) FROM users";
+    $r = @mysqli_query($dbc,$q);
+    $row = mysqli_fetch_array($r,MYSQLI_NUM);
+    $record = $row[0];
+    if($record>$display){
+      $pages = ceil ($record/$display);
+    }else{
+      $pages = 1;
+    }
+  }
+  if(isset($_GET['s']) && is_numeric($_GET['s'])){
+    $start = $_GET['s'];
+  }else{
+    $start = 0;
+  }
   // make the query
   $q = "SELECT CONCAT(last_name, ', ', first_name) AS name, DATE_FORMAT(registration_date,
-    '%M %d, %Y') AS dr, user_id FROM users ORDER BY registration_date";
+    '%M %d, %Y') AS dr, user_id FROM users ORDER BY registration_date LIMIT $start,$display";
   $r = @mysqli_query($dbc, $q);  
   $num = mysqli_num_rows($r);
   if($num>0){
@@ -35,6 +54,26 @@
           };
     echo '</tbody></table>';
     mysqli_free_result($r);
+    if($pages>1){
+      echo '<br><p>';
+      $current_page = ($start/$display) + 1;
+      if($current_page != 1) {
+        echo '<a href="view_users.php?s=' . ($start - $display) . '&p=' . $pages .'">Previous</a> ';
+      }
+      for ($i = 1; $i <= $pages; $i++) {
+        if ($i != $current_page) {
+          echo '<a href="view_users.php?s=' . (($display * ($i - 1))) . '&p=' . $pages .
+              '">' . $i . '</a> ';
+        } else {
+          echo $i . ' ';
+        }
+      }
+      if ($current_page != $pages) {
+        echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages .
+          '">Next</a>';
+      }
+      echo '</p>';
+    }
   }else { 
         echo '<p class=”error”>The current users could not be retrieved. We apologize for any
           inconvenience.</p>';
