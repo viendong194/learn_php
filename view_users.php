29997c1bf41c5a64a385ab5739ec60259e7c101e
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-08-28 19:30:41 +0900

    add order in view user

--- view_users.php
+++ view_users.php
@@ -23,9 +23,25 @@
   }else{
     $start = 0;
   }
+  $sort = isset($_GET['sort'])? $_GET['sort'] : 'rd';
+  switch($sort){
+    case 'fn':
+        $order_by = 'first_name ASC';
+        break;
+    case 'ln':
+        $order_by = 'last_name ASC';
+        break;
+    case 'rd':
+        $order_by = 'registration_date ASC';
+        break;
+    default:
+        $order_by = 'registration_date ASC';
+        $sort = 'rd';
+        break; 
+  };
   // make the query
-  $q = "SELECT CONCAT(last_name, ', ', first_name) AS name, DATE_FORMAT(registration_date,
-    '%M %d, %Y') AS dr, user_id FROM users ORDER BY registration_date LIMIT $start,$display";
+  $q = "SELECT last_name,first_name AS name, DATE_FORMAT(registration_date,
+    '%M %d, %Y') AS dr, user_id FROM users ORDER BY $order_by LIMIT $start,$display";
   $r = @mysqli_query($dbc, $q);  
   $num = mysqli_num_rows($r);
   if($num>0){
@@ -39,8 +55,9 @@
       <tr>
           <th align="left">Delete</th>
           <th align="left">Edit</th>
-          <th align="left">Name</th>
-          <th align="left">Date Registered</th>
+          <th align="left"><a href="view_users.php?sort=fn">First Name</a>/th>
+          <th align="left"><a href="view_users.php?sort=ln">Last Name</a></th>
+          <th align="left"><a href="view_users.php?sort=rrd">Date Registered</a></th>
       </tr>
       </thead>
       <tbody>
@@ -48,7 +65,8 @@
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
               echo '<tr><td align="lef"><a href="delete_user.php?id='.$row['user_id'].'">Delete</a></td>
                         <td align="left"><a href="edit_user.php?id='.$row['user_id'].'">Edit</a>
-                        </td><td align="lef">' . $row['name'] . '</td>
+                        </td><td align="lef">' . $row['first_name'] . '</td>
+                        </td><td align="lef">' . $row['last_name'] . '</td>
                         <td align="left">' . $row['dr'] .'</td>
                     </tr>';
           };
@@ -58,18 +76,18 @@
       echo '<br><p>';
       $current_page = ($start/$display) + 1;
       if($current_page != 1) {
-        echo '<a href="view_users.php?s=' . ($start - $display) . '&p=' . $pages .'">Previous</a> ';
+        echo '<a href="view_users.php?s=' . ($start - $display) . '&p=' . $pages .'&sort=' . $sort . '">Previous</a> ';
       }
       for ($i = 1; $i <= $pages; $i++) {
         if ($i != $current_page) {
-          echo '<a href="view_users.php?s=' . (($display * ($i - 1))) . '&p=' . $pages .
+          echo '<a href="view_users.php?s=' . (($display * ($i - 1))) . '&p=' . $pages .'&sort=' . $sort . 
               '">' . $i . '</a> ';
         } else {
           echo $i . ' ';
         }
       }
       if ($current_page != $pages) {
-        echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages .
+        echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages .'&sort=' . $sort . 
           '">Next</a>';
       }
       echo '</p>';
